99b5a4ce79fa1c1ac92a2923cbbe316c26f7d4bc
Felicitas Deutges <deutges.ironhack@gmail.com>
2018-10-17 17:03:21 +0200

    add of instructions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 console.log("Hello");
+// Iteration 1
+function goToPage(link) {
+  //Goal hide all the divs, with data page, except right one
+}
+//GoToMainPage
+goToPage("home")
+
+//Iteration 2: Listen for click events on a tags
+//Hint: event.preventDefault
